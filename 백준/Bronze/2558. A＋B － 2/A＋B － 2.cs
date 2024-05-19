b3d7eeab7bcb996c4108c579a8af48e46b96167b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-05-19 22:25:36 +0900

    [Bronze V] Title: A+B - 2, Time: 60 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine(A+B);
+        }
+    }
+}
