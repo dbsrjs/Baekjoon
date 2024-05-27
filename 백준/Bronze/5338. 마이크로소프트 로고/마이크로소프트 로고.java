c80f74d5d2d6141644a429da9a48b2ca6a524d18
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-05-28 00:01:43 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 112 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+    }
+}
+/*
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+* */
