f68589e2ce0ad6dabe48e51a8a62f9019c31c63c
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-05-28 00:07:41 +0900

    [Bronze V] Title: 콜센터, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,23 @@
+Console.WriteLine("     /~\\");
+Console.WriteLine("    ( oo|");
+Console.WriteLine("    _\\=/_");
+Console.WriteLine("   /  _  \\");
+Console.WriteLine("  //|/.\\|\\\\");
+Console.WriteLine(" ||  \\ /  ||");
+Console.WriteLine("============");
+Console.WriteLine("|          |");
+Console.WriteLine("|          |");
+Console.Write("|          |");
+
+/*
+     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |
+*/
