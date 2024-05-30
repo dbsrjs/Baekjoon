383255af3aa445e7e10093710103ff147f48756a
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-05-30 23:46:58 +0900

    [Bronze V] Title: NFC West vs North, Time: 56 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cs
@@ -0,0 +1,13 @@
+        Console.WriteLine("NFC West       W   L  T");
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("Seattle        13  3  0");
+        Console.WriteLine("San Francisco  12  4  0");
+        Console.WriteLine("Arizona        10  6  0");
+        Console.WriteLine("St. Louis      7   9  0");
+        Console.WriteLine("");
+        Console.WriteLine("NFC North      W   L  T");
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("Green Bay      8   7  1");
+        Console.WriteLine("Chicago        8   8  0");
+        Console.WriteLine("Detroit        7   9  0");
+        Console.WriteLine("Minnesota      5  10  1");
