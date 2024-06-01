99787d6eed5b68e15fd5a8bdece4ee39e571b858
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-01 23:11:39 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {        
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
