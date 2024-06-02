32ffe18154e313d4f6a87ee7fdb529d758206b78
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-02 23:37:53 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,8 @@
+string[] s = Console.ReadLine().Split();
+int a = int.Parse(s[0]);
+int b = int.Parse(s[1]);
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
