e1618da2fc90e0148b157c83e739d9527e877a33
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-07 02:17:17 +0900

    [Bronze V] Title: A×B, Time: 64 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -0,0 +1,9 @@
+while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == null) break;
+            string[] AB = input.Split();
+            int a = int.Parse(AB[0]);
+            int b = int.Parse(AB[1]);
+            Console.WriteLine(a * b);
+        }
