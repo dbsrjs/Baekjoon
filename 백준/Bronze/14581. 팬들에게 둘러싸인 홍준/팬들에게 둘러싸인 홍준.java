123fb406e8481105c0c4c3a570e7861d4cda89f2
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-09 22:30:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + name + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
