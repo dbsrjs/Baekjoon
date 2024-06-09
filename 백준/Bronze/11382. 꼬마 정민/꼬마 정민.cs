89d744dc7656c33ca88c0ad458c32234aa753de8
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-09 22:13:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,7 @@
+string input = Console.ReadLine();
+string[] token = input.Split();
+
+long A = long.Parse(token[0]);
+long B = long.Parse(token[1]);
+long C = long.Parse(token[2]);
+Console.WriteLine(A + B + C);
