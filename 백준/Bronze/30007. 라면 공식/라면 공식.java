0067b48c50a0513ab6dc68f4aefc3712663cea94
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-10 20:46:49 +0900

    Update 라면 공식.java

--- 백준/Bronze/30007. 라면 공식/라면 공식.java
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -1,4 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] str = sc.nextLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            int X = Integer.parseInt(str[2]);
+            System.out.println(A * (X - 1) + B);
+        }
+
+        sc.close();
+    }
+}
+
+/*import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
@@ -16,4 +36,4 @@ public class Main {
             System.out.println(A * (X - 1) + B);
         }
     }
-}
+}*/
