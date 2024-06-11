caccdfeb45a53f82798d9f068fec88db0be9dcbd
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-11 10:10:40 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            String[] str = sc.nextLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            System.out.println(A+B);
+    }
+}
