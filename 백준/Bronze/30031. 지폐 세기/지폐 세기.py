822fb79d0c9b4983a9ab356416fea6b32b893635
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-14 21:59:21 +0900

    [Bronze IV] Title: 지폐 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30031. 지폐 세기/지폐 세기.py
@@ -0,0 +1,15 @@
+N = int(input())
+result = 0
+
+for _ in range(N):
+    w, h = map(int, input().split())
+    if w == 136:
+        result += 1000
+    elif w == 142:
+        result += 5000
+    elif w == 148:
+        result += 10000
+    elif w == 154:
+        result += 50000
+
+print(result)
