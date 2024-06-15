8a24ac06cd0684a22b347e350e7a20a004ea0ff2
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-15 23:50:38 +0900

    [Bronze V] Title: 연세대학교, Time: 200 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if(input == 0)
+            System.out.println("YONSEI");
+        else if(input == 1)
+            System.out.println("Leading the Way to the Future");
+    }
+}
