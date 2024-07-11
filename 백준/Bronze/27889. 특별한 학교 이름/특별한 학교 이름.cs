57599567c0aa46b8568f54e3e97f68845e0e3c3c
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-11 09:44:16 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 52 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,12 @@
+using System;
+
+string str = Console.ReadLine();
+
+if (str.Equals("NLCS"))
+    Console.WriteLine("North London Collegiate School");
+else if (str.Equals("BHA"))
+    Console.WriteLine("Branksome Hall Asia");
+else if (str.Equals("KIS"))
+    Console.WriteLine("Korea International School");
+else if (str.Equals("SJA"))
+    Console.WriteLine("St. Johnsbury Academy");
