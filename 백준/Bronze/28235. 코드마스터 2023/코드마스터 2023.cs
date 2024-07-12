b00c6b3ceb1c5215a1b3e0f4efb83b58edc0729b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-12 22:52:47 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 52 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,18 @@
+String a = Console.ReadLine();
+
+if (a.Equals("SONGDO"))
+{
+    Console.WriteLine("HIGHSCHOOL");
+}
+else if (a.Equals("CODE"))
+{
+    Console.WriteLine("MASTER");
+}
+else if (a.Equals("2023"))
+{
+    Console.WriteLine("0611");
+}
+else if (a.Equals("ALGORITHM"))
+{
+    Console.WriteLine("CONTEST");
+}
