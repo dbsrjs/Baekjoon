8ff7afd5f42a830f46290eaf44e6782911080c45
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-13 20:34:36 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 52 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cs
@@ -0,0 +1,12 @@
+string s = Console.ReadLine();
+
+if (s.Equals("M"))
+    Console.WriteLine("MatKor");
+else if(s.Equals("W"))
+    Console.WriteLine("WiCys");
+else if (s.Equals("C"))
+    Console.WriteLine("CyKor");
+else if (s.Equals("A"))
+    Console.WriteLine("AlKor");
+else if (s.Equals("$"))
+    Console.WriteLine("$clear");
