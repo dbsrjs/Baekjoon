51047ccd27361f9fd0dc8899de22ce335519f087
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-29 23:43:39 +0900

    [Bronze V] Title: Rectangles, Time: 76 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cs
@@ -0,0 +1,10 @@
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write("*");
+    Console.WriteLine();
+}
+    
