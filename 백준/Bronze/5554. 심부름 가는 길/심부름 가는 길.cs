c77866d4574b2c4dec1f3c251160c9d20f77c194
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-07 15:06:22 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cs
@@ -0,0 +1,11 @@
+using System;
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+
+int result = a + b + c + d;
+
+Console.WriteLine(result / 60);
+Console.WriteLine(result % 60);
