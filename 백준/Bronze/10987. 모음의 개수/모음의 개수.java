4892123d57986a4ebbd124cd1bf88bc6f8fcb66e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-12 22:09:13 +0900

    [Bronze IV] Title: 모음의 개수, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char t = str.charAt(i);
+			if (t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u')
+				++count;
+		}
+		System.out.println(count);
+		sc.close();
+	}
+}
