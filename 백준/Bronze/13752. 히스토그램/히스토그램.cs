5d47ac79e96001f208dbcb25f620c9d1bedb9ec9
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-22 21:53:11 +0900

    [Bronze IV] Title: 히스토그램, Time: 268 ms, Memory: 6600 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cs
@@ -0,0 +1,9 @@
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    int k = int.Parse(Console.ReadLine());
+    for (int j = 0; j < k; j++)
+        Console.Write("=");
+    Console.WriteLine();
+}
