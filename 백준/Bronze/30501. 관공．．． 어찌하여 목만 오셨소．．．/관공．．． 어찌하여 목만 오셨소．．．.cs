168983a02298d013d91a5923e68301eb3f8b2b67
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-01-13 23:34:23 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 48 ms, Memory: 5604 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cs
@@ -0,0 +1,11 @@
+int cnt = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < cnt; i++)
+{
+    string str = Console.ReadLine();
+
+    if(str.Contains("S"))
+    {
+        Console.WriteLine(str);
+    }
+}
