3bc00bde444c07dd1225454750db28b9aa741215
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-01-29 23:49:30 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int lv = sc.nextInt();
+        String sign = sc.next();
+
+        if (sign.equals("miss"))
+            System.out.println(0);
+        else if (sign.equals("bad"))
+            System.out.println(200 * lv);
+        else if (sign.equals("cool"))
+            System.out.println(400 * lv);
+        else if (sign.equals("great"))
+            System.out.println(600 * lv);
+        else if (sign.equals("perfect"))
+            System.out.println(1000 * lv);
+    }
+}
