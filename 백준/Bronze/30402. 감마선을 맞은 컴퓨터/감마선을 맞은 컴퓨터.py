ee558ed0b309619de641bfd5d6e787f993163275
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-02-03 23:21:11 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+for _ in range(15):
+    pic = list(input().split())
+
+    if "w" in pic:
+        print("chunbae")
+        break
+    elif "b" in pic:
+        print("nabi")
+        break
+    elif "g" in pic:
+        print("yeongcheol")
+        break
