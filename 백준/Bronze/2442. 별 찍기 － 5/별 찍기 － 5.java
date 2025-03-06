91f4f3597b5e3bf4b25918ed71834aab1794f4aa
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-03-06 21:23:33 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 312 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
