19511c0af2327d30ed27cf08401137fd9de6e55b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-05-08 10:13:13 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 216 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int m1 = sc.nextInt() - 1;
+            int m2 = sc.nextInt() - 1;
+
+            while (m1 < m2) {
+                int temp = nums[m1];
+                nums[m1] = nums[m2];
+                nums[m2] = temp;
+                m1++;
+                m2--;
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
