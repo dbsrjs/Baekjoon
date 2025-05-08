adf67648c1bfb5413af6a08c2d3b0e903a576fb9
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-05-08 11:04:03 +0900

    [Bronze I] Title: 약수, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -4,14 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int n = sc.nextInt(); // 약수의 개수
+        int[] divisors = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            if (num < min) min = num;
-            if (num > max) max = num;
+            divisors[i] = sc.nextInt();
+        }
+
+        int min = divisors[0];
+        int max = divisors[0];
+
+        for (int i = 1; i < n; i++) {
+            if (divisors[i] < min) min = divisors[i];
+            if (divisors[i] > max) max = divisors[i];
         }
 
         System.out.println(min * max);
