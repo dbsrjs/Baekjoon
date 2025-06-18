9cd46d2a9f1fc61bba2da2504955a91b14c797fa
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2025-06-18 09:32:51 +0900

    [Bronze III] Title: 지능형 기차, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cs
@@ -0,0 +1,27 @@
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0; //현재 기차 인원 수
+            int maxCnt = 0; //최대 기차 인원 수
+
+            for(int i = 0; i < 4; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int outCnt = int.Parse(input[0]); //내리는 인원 수
+                int inCnt = int.Parse(input[1]); //타는 인원 수
+
+                cnt -= outCnt;
+                cnt += inCnt;
+                if (cnt > maxCnt) //최대 기차 인원 수 갱신
+                {
+                    maxCnt = cnt;
+                }
+            }
+
+            Console.WriteLine(maxCnt);
+        }
+    }
+}
